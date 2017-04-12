cb7c1731a34cf09e1a53e658583941d4e36cbace
Deniss <den.b@itservicesgroup.co.uk>
2017-04-12 15:24:31 +0100

    Add Routes for search and root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'api#index'
+  post '/api/search' , controller: 'api', action: 'search'
+  get '/api/search' , controller: 'api', action: 'search'
 end
